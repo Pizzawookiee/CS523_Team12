22dea541ce4e0240b796f957b0addbdd543b3016
David Lee <dtlee@bu.edu>
2024-12-01 14:19:33 -0500

    Add files via upload

--- FUSER_v2_to_v3_fix.py
+++ FUSER_v2_to_v3_fix.py
@@ -11,15 +11,16 @@ pd.set_option('display.max_columns', None)
 actual = pd.DataFrame(columns = ['ID', 'Value'])
 airports = ['KDEN', 'KMEM', 'KORD', 'KSEA', 'KDFW', 'KPHX', 'KCLT', 'KATL', 'KMIA', 'KJFK']
 
-train_or_test = 'test'
+train_or_test = 'train'
 
 for a in airports:
     
    
-    v1 = pd.read_parquet(a+"_data_"+train_or_test+".parquet")
+    
     v2 = pd.read_parquet(a+"_data_"+train_or_test+"_v2.parquet")
-     
-    v1_is_arrival = v1[['gufi', 'is_arrival']].drop_duplicates()
-    v3 = pd.merge(v1_is_arrival, v2, on='gufi')
+    
+    
+    v3 = v2
+    v3['is_arrival'] = ~v3["arrival_runway_actual_time"].isna()
     write(a+"_data_"+train_or_test+"_v3.parquet", v3, compression='brotli', write_index=False)
     print("Finished processing v3 FUSER for " + a) 
