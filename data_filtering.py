d8dd286f600f65ac67d8f627b9af64895ae4e77d
David Lee <dtlee@bu.edu>
2024-12-13 21:57:18 -0500

    cleaned up commented out code

--- data_filtering.py
+++ data_filtering.py
@@ -6,9 +6,9 @@ import pandas as pd
 from fastparquet import write
 
 
-'''
-STAGE 1: retrieve all TAF data relating to only arrival airports
-'''
+#script that demonstrates how we filtered METAR (and TAF) data to only use values from airports that are in FUSER data
+
+
 folder = 'METAR_parquet_batches_train/METAR_parquet_batches_train'
 airports = ['KDEN', 'KMEM', 'KORD', 'KSEA', 'KDFW', 'KPHX', 'KCLT', 'KATL', 'KMIA', 'KJFK']
 
@@ -20,13 +20,7 @@ for b in batch_filenames:
     df = pd.read_parquet(os.path.join(folder, b))
     result = pd.concat([result, df.loc[df['station_id'].isin(airports)]], ignore_index = True)
     del df
-'''
-result = pd.DataFrame()
-airports = ['KDEN', 'KMEM', 'KORD', 'KSEA', 'KDFW', 'KPHX', 'KCLT', 'KATL', 'KMIA', 'KJFK']
-df = pd.read_parquet('metar_test_data.parquet')
-result = pd.concat([result, df.loc[df['station_id'].isin(airports)]], ignore_index = True)
-del df
-'''
+
 
 write('METAR_train' + '_only_arrival_airports.parquet', result, compression='brotli', write_index=False)
 
